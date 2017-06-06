28fcf4e8ce4fab435a8513f29f2e094be4412891
jahnava <jahnavalmt@gmail.com>
2017-06-05 22:19:43 -0400

    Set ball interval to repeat and gave specific coordinantes for ball to move. Got rid of ball shadow and set wall boundaries for ball to bounce off of.

--- public/js/app.js
+++ public/js/app.js
@@ -1,25 +1,64 @@
+
 // referancing the canvas element variable and creating a variable named ctx to store 2D rendering context.
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
+//defining the starting point for the ball
+var x = canvas.width/2;
+var y= canvas.height-30;
+var dx = 2;
+var dy = -2;
+//holds radius of circle
+var ballRadius = 10;
+
+function drawBall() {
+  //now creating a circle
+  //the arc takes six parameters: x & y coordinates of the arcs center, arc radius, start & end angles, direction of drawing (false for clockwise & true for counter-clockwise)
+  ctx.beginPath();
+  ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+  ctx.fillStyle = "#0095DD";
+  ctx.fill();
+  ctx.closePath();
+  //setting wall boundaries for ball to bounce off of
+  //top wall
+  if(y + dy < 0) {
+    dy = -dy;
+  }
+  //bottom wall
+  if(y + dy > canvas.height || y + dy < 0) {
+    dy = -dy;
+  }
+  //left and right walls
+  if(x + dx > canvas.width || x + dx < 0) {
+    dx = -dx;
+  }
+}
+
+function draw() {
+  ctx.clearRect(0,0, canvas.width, canvas.height);
+  drawBall();
+  x += dx;
+  y += dy;
+}
+//set interval creates a loop that will be called every 10 millaseconds
+setInterval(draw,10);
+
+
+
+
+
+
+
 
 // using the ctx variable to create a red square on the canvas between the beginPath and closePath
-ctx.beginPath();
-ctx.rect(20, 40, 50, 50);
-ctx.fillStyle = "red";
-ctx.fill();
-ctx.closePath();
-
-//now creating a circle
-//the arc takes six parameters: x & y coordinates of the arcs center, arc radius, start & end angles, direction of drawing (false for clockwise & true for counter-clockwise)
-ctx.beginPath();
-ctx.arc(240, 160, 20, 0, Math.PI*2, false);
-ctx.fillStyle = "green";
-ctx.fill();
-ctx.closePath();
+// ctx.beginPath();
+// ctx.rect(20, 40, 50, 50);
+// ctx.fillStyle = "red";
+// ctx.fill();
+// ctx.closePath();
 
 //adding stroke for stroke outline
-ctx.beginPath();
-ctx.rect(160, 10, 100, 40);
-ctx.strokeStyle = "rgba(0, 0, 225, 0.5)";
-ctx.stroke();
-ctx.closePath();
+// ctx.beginPath();
+// ctx.rect(160, 15, 200, 40);
+// ctx.strokeStyle = "rgba(0, 0, 225, 0.5)";
+// ctx.stroke();
+// ctx.closePath();
