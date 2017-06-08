b2e1e10c138567ed25e80f0f9a954e0a7c7bef4d
jahnava <jahnavalmt@gmail.com>
2017-06-08 18:01:55 -0400

    Added bricks for the ball to accumulate points. Also, added score keeper, win and lose functions, and alert box.

--- public/js/app.js
+++ public/js/app.js
@@ -1,66 +1,140 @@
-
-// referancing the canvas element variable and creating a variable named ctx to store 2D rendering context.
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
-//defining the starting point for the ball
+var ballRadius = 10;
 var x = canvas.width/2;
-var y= canvas.height-30;
+var y = canvas.height-30;
 var dx = 2;
 var dy = -2;
-//holds radius of circle
-var ballRadius = 10;
+var paddleHeight = 10;
+var paddleWidth = 75;
+var paddleX = (canvas.width-paddleWidth)/2;
+var rightPressed = false;
+var leftPressed = false;
+var brickRowCount = 5;
+var brickColumnCount = 3;
+var brickWidth = 75;
+var brickHeight = 20;
+var brickPadding = 10;
+var brickOffsetTop = 30;
+var brickOffsetLeft = 30;
+var score = 0;
+
+var bricks = [];
+for(c=0; c<brickColumnCount; c++) {
+    bricks[c] = [];
+    for(r=0; r<brickRowCount; r++) {
+        bricks[c][r] = { x: 0, y: 0, status: 1 };
+    }
+}
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function keyDownHandler(e) {
+    if(e.keyCode == 39) {
+        rightPressed = true;
+    }
+    else if(e.keyCode == 37) {
+        leftPressed = true;
+    }
+}
+function keyUpHandler(e) {
+    if(e.keyCode == 39) {
+        rightPressed = false;
+    }
+    else if(e.keyCode == 37) {
+        leftPressed = false;
+    }
+}
+function collisionDetection() {
+    for(c=0; c<brickColumnCount; c++) {
+        for(r=0; r<brickRowCount; r++) {
+            var b = bricks[c][r];
+            if(b.status == 1) {
+                if(x > b.x && x < b.x+brickWidth && y > b.y && y < b.y+brickHeight) {
+                    dy = -dy;
+                    b.status = 0;
+                    score++;
+                    if(score == brickRowCount*brickColumnCount) {
+                        alert("YOU WIN, CONGRATS!");
+                        document.location.reload();
+                    }
+                }
+            }
+        }
+    }
+}
 
 function drawBall() {
-  //now creating a circle
-  //the arc takes six parameters: x & y coordinates of the arcs center, arc radius, start & end angles, direction of drawing (false for clockwise & true for counter-clockwise)
-  ctx.beginPath();
-  ctx.arc(x, y, ballRadius, 0, Math.PI*2);
-  ctx.fillStyle = "#0095DD";
-  ctx.fill();
-  ctx.closePath();
+    ctx.beginPath();
+    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+function drawBricks() {
+    for(c=0; c<brickColumnCount; c++) {
+        for(r=0; r<brickRowCount; r++) {
+            if(bricks[c][r].status == 1) {
+                var brickX = (r*(brickWidth+brickPadding))+brickOffsetLeft;
+                var brickY = (c*(brickHeight+brickPadding))+brickOffsetTop;
+                bricks[c][r].x = brickX;
+                bricks[c][r].y = brickY;
+                ctx.beginPath();
+                ctx.rect(brickX, brickY, brickWidth, brickHeight);
+                ctx.fillStyle = "chartreuse";
+                ctx.fill();
+                ctx.closePath();
+            }
+        }
+    }
+}
+function drawScore() {
+    ctx.font = "16px Arial";
+    ctx.fillStyle = "#0095DD";
+    ctx.fillText("Score: "+score, 8, 20);
 }
 
 function draw() {
-  ctx.clearRect(0,0, canvas.width, canvas.height);
-  drawBall();
-  //setting wall boundaries for ball to bounce off of
-  //top wall
-  if(y + dy < 0) {
-    dy = -dy;
-  }
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBricks();
+    drawBall();
+    drawPaddle();
+    drawScore();
+    collisionDetection();
 
-  //bottom wall
-  if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
-    dy = -dy;
-  }
-  //left and right walls
-  if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
-    dx = -dx;
-  }
-  
-  x += dx;
-  y += dy;
+    if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+        dx = -dx;
+    }
+    if(y + dy < ballRadius) {
+        dy = -dy;
+    }
+    else if(y + dy > canvas.height-ballRadius) {
+        if(x > paddleX && x < paddleX + paddleWidth) {
+            dy = -dy;
+        }
+        else {
+            alert("GAME OVER");
+            document.location.reload();
+        }
+    }
+
+    if(rightPressed && paddleX < canvas.width-paddleWidth) {
+        paddleX += 7;
+    }
+    else if(leftPressed && paddleX > 0) {
+        paddleX -= 7;
+    }
+
+    x += dx;
+    y += dy;
 }
-//set interval creates a loop that will be called every 10 millaseconds
-setInterval(draw,10);
 
-
-
-
-
-
-
-
-// using the ctx variable to create a red square on the canvas between the beginPath and closePath
-// ctx.beginPath();
-// ctx.rect(20, 40, 50, 50);
-// ctx.fillStyle = "red";
-// ctx.fill();
-// ctx.closePath();
-
-//adding stroke for stroke outline
-// ctx.beginPath();
-// ctx.rect(160, 15, 200, 40);
-// ctx.strokeStyle = "rgba(0, 0, 225, 0.5)";
-// ctx.stroke();
-// ctx.closePath();
+setInterval(draw, 10);
